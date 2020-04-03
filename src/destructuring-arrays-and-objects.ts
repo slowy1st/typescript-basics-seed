c75ede5546dd308290134426a504c2251d92e288
slowy <slowfly1st@gmail.com>
2020-04-03 16:42:36 +0200

    destructuring-arrays-and-objects

---
+++ src/destructuring-arrays-and-objects.ts
@@ -0,0 +1,43 @@
+const component = {
+    name: 'GPU',
+    vendor: 'ASUS'
+};
+
+function order(component: any) {
+    console.log(component);
+}
+
+order(component);
+
+// with destructuring params
+function order2({name, vendor}) {
+    console.log(name, vendor);
+}
+
+order2(component);
+
+// rename at the same time!
+function order3({name: gpuName, vendor: vendorName}) {
+    return {gpuName, vendorName};
+}
+
+order3(component);
+
+// also works for declaration!
+
+const {vendorName} = order3(component);
+console.log(vendorName);
+
+// array destructuring
+const vendors = ['ASUS', 'Intel', 'Samsung'];
+
+const [first, second, third] = vendors;
+console.log(first);
+console.log(second);
+console.log(third);
+
+function logVendors([first, second, third]: any) {
+    console.log(first, second, third);
+}
+
+logVendors(vendors);
