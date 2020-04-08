9bb2b0363748100cd048c81de95773e861609a94
slowy <slowfly1st@gmail.com>
2020-04-08 17:31:48 +0200

    fixes compilation error

--- src/types-boolean.ts
+++ src/types-boolean.ts
@@ -1,10 +1,10 @@
-const pizzas: number = 1;
+const pizzas7: number = 1;
 
 function offerDiscount(orders: number): boolean {
     return orders >= 3;
 }
 
-if (offerDiscount(pizzas)) {
+if (offerDiscount(pizzas7)) {
     console.log(`You're entitled to a discount!`)
 } else {
     console.log(`Order more than 3 pizzas for a discount`);
