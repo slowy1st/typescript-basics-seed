cffa22ae4834383fdda80557cc34d5376a3f6e82
slowy <slowfly1st@gmail.com>
2020-04-14 13:08:35 +0200

    fix naming

--- src/types-typed-functions-and-default-params.ts
+++ src/types-typed-functions-and-default-params.ts
@@ -1,24 +1,24 @@
 // ? == optional
-let sumOrder3: (price: number, quantity?: number) => number;
+let sumOrder7: (price: number, quantity?: number) => number;
 
-// old way
-sumOrder3 = (x, y) => {
-    if (!y) {
-        return x;
-    }
-
-    return x * y;
-};
-
-// with ||
-sumOrder3 = (x, y) => {
-    const newY = y || 1;
-
-    return x * newY;
-};
+// // old way
+// sumOrder3 = (x, y) => {
+//     if (!y) {
+//         return x;
+//     }
+//
+//     return x * y;
+// };
+//
+// // with ||
+// sumOrder3 = (x, y) => {
+//     const newY = y || 1;
+//
+//     return x * newY;
+// };
 
 // with defaultValue
-sumOrder3 = (x, y = 1) => x * y;
+sumOrder7 = (x, y = 1) => x * y;
 
-console.log(sumOrder3(13));
-console.log(sumOrder3(13, 12));
+console.log(sumOrder7(13));
+console.log(sumOrder7(13, 12));
