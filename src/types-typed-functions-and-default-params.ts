839b93925ac16755b606581b2f422f487250724f
slowy <slowfly1st@gmail.com>
2020-04-14 12:55:12 +0200

    types - typed functions and default params

---
+++ src/types-typed-functions-and-default-params.ts
@@ -0,0 +1,24 @@
+// ? == optional
+let sumOrder3: (price: number, quantity?: number) => number;
+
+// old way
+sumOrder3 = (x, y) => {
+    if (y) {
+        return x;
+    }
+
+    return x * y;
+};
+
+// with ||
+sumOrder3 = (x, y) => {
+    const newY = y || 1;
+
+    return x * newY;
+};
+
+// with defaultValue
+sumOrder3 = (x, y = 1) => x * y;
+
+console.log(sumOrder3(13));
+console.log(sumOrder3(13, 12));
