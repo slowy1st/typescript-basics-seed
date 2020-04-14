2e054b833a8239765d6ad289e0d19a9c89ff612d
slowy <slowfly1st@gmail.com>
2020-04-14 13:09:33 +0200

    array-types and generics

---
+++ src/types-array-types-and-generics.ts
@@ -0,0 +1,11 @@
+const sizes = ['small', 'medium', 'large'];
+// with explicit type
+const sizes2: string[] = ['small', 'medium', 'large'];
+
+// w/ generic type
+let toppings: string[];
+
+toppings = ['pepperon', 'bacon'];
+
+let toppings2: Array<string>;
+toppings2 = ['blub'];
